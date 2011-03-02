4c9ce3619429dc592d1e4e43fb70939860e5b60b
Will Moffat <will_git@hamstersoup.com>
2011-03-01 21:39:17 -0800

    Added a map.
    Unfortunately I couldn't find a CSS unit for one character width, so I had to calcualte it.
    
    I'm not happy about the number of lines of code added.

--- tinymmo.js
+++ tinymmo.js
@@ -12,6 +12,34 @@ server = http.createServer(function(req, res){
 });
 server.listen(8080);
 
+
+var world = (function() {
+  var map=[], maxX=40, maxY=20;
+
+  var item = {
+    EMPTY:' ',
+    WALL: '#',
+    TRAP: 'v',
+    GOLD: '$'
+  };
+
+  function chooseCell(x,y) {
+    if ( x===0 || x===maxX || y===0 || y===maxY ) return item.WALL;
+    if (Math.random() > 0.97) return item.TRAP;
+    if (Math.random() > 0.97) return item.GOLD;
+    return item.EMPTY;
+  }
+
+  // init
+  for (var y = 0; y <= maxY; y++) {
+    map[y]=[];
+    for (var x = 0; x <= maxX; x++) {
+      map[y][x] = chooseCell(x,y);
+    }
+  }
+  return { map:map, item:item, maxX:maxX, maxY:maxY };
+})();
+
 //function makeMessage(t, args) { return {t:t }
 
 function updatePlayer(client) {
@@ -41,33 +69,45 @@ function checkCollision(movedClient, client) {
   //console.log(movedClient.pid, "   ", client.pid);
 }
 
+// client actions
+var actions = {
+  "move" : function(client, msg) {
+    var newX = client.x + msg.dx,
+        newY = client.y + msg.dy;
+    if (world.map[newY][newX] === world.item.EMPTY) {
+      client.x = newX;
+      client.y = newY;
+      forEachClient(function(c) { checkCollision(client, c); });
+      updatePlayer(client);
+    }
+    // TODO: handle the other item types
+  }
+};
+
+function messageHandler(client, msg) {
+  console.log('Received: '+msg.t+' from client:'+client.pid);
+  if (msg.t in actions) {
+    actions[msg.t](client, msg);
+  } else {
+    console.error("unknown message type: ", msg);
+  }
+}
+
 var socket = io.listen(server);
+
+// New client connects
 socket.on('connection', function(client) {
   client.pid = pid;
-  client.x = Math.round(Math.random()*12);
-  client.y = Math.round(Math.random()*6);
+  client.x = Math.round(Math.random() * world.maxX);
+  client.y = Math.round(Math.random() * world.maxY);
   pid++;
 
-  client.send({t: "init", pid: client.pid});
+  client.send({t: "init", pid: client.pid, map:world.map});
   forEachClient(function(c) {
     updatePlayer(c);
   });
 
-  function messageHandler(msg) {
-    console.log("message: ", msg, "  ", client.pid);
-
-    switch (msg.t) {
-      case "move":
-        client.x += msg.dx;
-        client.y += msg.dy;
-        forEachClient(function(c) { checkCollision(client, c); });
-        updatePlayer(client);
-        break;
-    }
-  }
-
-  // new client is here!
-  client.on('message', messageHandler);
+  client.on('message', function(m) { messageHandler(client,m); });
 
   client.on('disconnect', function() { killPlayer(client); });
 });
